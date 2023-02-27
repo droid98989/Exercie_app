0fd5088915000162d9d8ddeab503856e26759e76
droid98989 <adithyapm16e@student.mes.ac.in>
2023-02-27 10:37:54 -0500

    Deployed website

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -20,7 +20,7 @@ export const BmiOptions = {
   method: 'GET',
   url: 'https://bmi10.p.rapidapi.com/bmicommon',
   headers: {
-    'X-RapidAPI-Key': 'b4da7dfb16msh4b6d12d85dccd93p1f864cjsna625f2e2b4a9',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'bmi10.p.rapidapi.com'
   }
 };
